d407e4e1e86d5c3470ca9dfb7ff6de3b48752a69
Drew Mitchell <hi@callmedrew.com>
2018-12-19 23:09:08 -0600

    Set <title> to have different text according to page.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -15,4 +15,13 @@ module ApplicationHelper
   def print_bearing(bearing_angle)
     Geocoder::Calculations.compass_point(bearing_angle)
   end
+
+  def full_title(page_title = "")
+    base_title = "Poo at CWRU"
+    if page_title.empty?
+      base_title
+    else
+      page_title + " | " + base_title
+    end
+  end
 end
