a73f3c08244d2c69024575cd111232b26f95cd6b
Drew Mitchell <hi@callmedrew.com>
2019-08-21 20:01:56 -0400

    Use Arel.sql on hardcoded SQL.

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,6 @@
 class WelcomeController < ApplicationController
   def index
-    @most_popular_buildings = Building.left_joins(:toilet).group(:id).order('COUNT(toilets.id) DESC').limit(5)
-    @random_buildings = Building.all.order("random()").limit(5)
+    @most_popular_buildings = Building.left_joins(:toilet).group(:id).order(Arel.sql("COUNT(toilets.id) DESC")).limit(5)
+    @random_buildings = Building.all.order(Arel.sql("random()")).limit(5)
   end
 end
